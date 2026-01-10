f3bf7bb106bebd6442e44994c9620a1016e1ea36
pupungeggang2 <kyongkyong2892@gmail.com>
2026-01-10 22:08:25 +0900

    Start

---
+++ randomwalk/src/script/ui.py
@@ -0,0 +1,15 @@
+class UI():
+    title: dict[str, list[int]] = {
+        'button_start': [],
+        'text_start': [],
+        'button_4_dir': [],
+        'text_4_dir': [],
+        'button_all': [],
+        'text_all': [],
+
+        'text_step_size': [20, 20],
+        'text_step_num': [320, 20],
+
+        'area_step_size': [20, 20, 160, 40],
+        'area_step_num': [200, 20, 160, 40]
+    }
